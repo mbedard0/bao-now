82d9d587793730d9a11917f9af6da986f3d1dc79
Meiling Bedard <meiling.bedard@gmail.com>
2021-12-31 12:30:52 -0500

    add styling and dumpling model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Dumpling(models.Model):
+  name = models.CharField(max_length=40)
+  filling = models.CharField(max_length=300)
+  cook_type = models.CharField(max_length=100)
+  country = models.CharField(max_length=100)
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Dumpling 
 # Create your views here.
 
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
-  return render(render, 'about.html')
+  return render(request, 'about.html')
+
+def index(request):
+  dumplings = Dumpling.objects.all()
+  return render(request, 'dumplings/index.html', {'dumplings': dumplings})
